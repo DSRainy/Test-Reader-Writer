adbe3b43a6e964c03f83522ea98e2e9da220eac1
Rainy <Rainy@Rainy-PC>
2014-08-07 13:59:47 +0700

    Stream Complete

--- src/main/java/com/rainy/test/testio/TestIOStream.java
+++ src/main/java/com/rainy/test/testio/TestIOStream.java
@@ -5,11 +5,13 @@
  */
 package com.rainy.test.testio;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,22 +23,40 @@ public class TestIOStream {
 
     public static void main(String[] args) {
 
-//        try (FileInputStream inputFile = new FileInputStream(new File("D:\\test\\test.txt"));
-//                FileOutputStream outputFile = new FileOutputStream(new File("D:\\test\\outputstream.txt"));) {
-//
-//            int ch = 0;
-//
-//            while ((ch = inputFile.read()) != -1) {
-//                System.out.print((char) ch);
-//                outputFile.write((char) ch);
-//            }
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(TestReaderWriter.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Logger.getLogger(TestReaderWriter.class.getName()).log(Level.SEVERE, null, ex);
-//        } finally {
-//
-//        }
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+//        InputStreamReader inputStreamReader;
+        try {
+            fileInputStream = new FileInputStream("D:\\test\\test.txt");
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+//            inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            fileOutputStream = new FileOutputStream("D:\\test\\outputstream.txt");
+            bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            
+            int c = 0;
+            
+            while((c=bufferedInputStream.read())!=-1){
+                bufferedOutputStream.write((char) c);
+                System.out.print((char) c);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TestIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TestIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            try {
+                bufferedInputStream.close();
+                bufferedOutputStream.close();
+                fileInputStream.close();
+                fileOutputStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(TestIOStream.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
 
     }
 
