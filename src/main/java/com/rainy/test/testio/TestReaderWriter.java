bf757a6f4110244b0be04e6036d96ebf4a40c768
Rainy <Rainy@Rainy-PC>
2014-08-07 12:07:47 +0700

    Reader Writer  - can read thai  - can write thai
    Stream
     - no complete

--- src/main/java/com/rainy/test/testio/TestReaderWriter.java
+++ src/main/java/com/rainy/test/testio/TestReaderWriter.java
@@ -14,9 +14,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
